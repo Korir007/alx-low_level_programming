99ba4092d47a53eb857ad02026ba3498df84aba4
dann <dann7@debian-BULLSEYE-live-builder-AMD64>
2023-02-17 13:44:17 +0300

    9th challenge

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Prints a quote
+ * 
+ * Return: 0(Success)
+ * 
+*/
+
+int main(void)
+{
+    char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+    write(1, quo, 59);
+    return(1);
+}
