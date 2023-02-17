60b2548ab2aaa840894f0ffd886f24343d257813
dann <dann7@debian-BULLSEYE-live-builder-AMD64>
2023-02-17 13:53:50 +0300

    9th challenge

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,8 @@
 
 int main(void)
 {
-    char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    char quo[] ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-    write(1, quo, 59);
-    return(1);
+    write(1,quo,59);
+    return( 1 );
 }
