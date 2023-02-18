2d6afcf2bb600afa0803cdf044dad166947bd602
dann <dann7@debian-BULLSEYE-live-builder-AMD64>
2023-02-18 09:49:38 +0300

    9th challenge

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
-#include <unistd.h>
-/**
- * main - Prints a quote
- *
- * Return: 0(Success)
- *
-*/
+#include <string.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char string[60];
+	size_t i = 0;
 
-    write(1, quo, 59);
-    return (1);
+	strcpy(string, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	while  (i < strlen(string))
+	{
+		putc(string[i], stderr);
+		++i;
+	}
+	putc('\n', stderr);
+	return (1);
 }
