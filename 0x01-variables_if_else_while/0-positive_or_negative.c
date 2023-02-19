310e31189397a03f10ad024b227190f9814a577d
dann <dann7@debian-BULLSEYE-live-builder-AMD64>
2023-02-19 13:21:00 +0300

    11th challenge

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,22 +1,23 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include <stdio.h>
 /**
  * main - assign a random number to int n
- * Return: 0{success}
-*/
+ * each time it executes and prints it
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int n;
+	int n;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    if (n > 0)
-            printf("%d is positive\n",n);
-    else if (n == 0)
-            printf("%d is zero\n", n);
-    else
-            printf("%d is negative\n", n);
-    return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+	return (0);
 }
