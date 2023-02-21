7273b23295c529df96f9e70b31d60217863dd9fd
dann <dann7@debian-BULLSEYE-live-builder-AMD64>
2023-02-21 23:31:49 +0300

    6th challenge

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,21 +1,20 @@
-
 #include "main.h"
 
 /**
  * print_sign - return 0 letter not lowercase, 1 letter lowercase
  *
- * @n: the int to print
+ * @j: the int to print
  *  Return: Always 0.
   */
-int print_sign(int n)
+int print_sign(int j)
 {
-	if (n > 0)
+	if (j > 0)
 	{
 		_putchar ('+');
 		return (1);
 	}
 
-	else if (n == 0)
+	else if (j == 0)
 	{
 		_putchar ('0');
 		return (0);
