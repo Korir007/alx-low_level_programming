b5f8c5405a3558fbd648e16274d88643df5752fe
dann <dann7@debian-BULLSEYE-live-builder-AMD64>
2023-02-23 10:41:30 +0300

    5th challenge

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,17 @@
 /**
  * _isalpha - checks if a character is an alphabetic letter
- *
- * @c: the character to check
+ * @c: character to check
  *
  * Return: 1 if the character is an alphabetic letter, 0 otherwise
  */
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
