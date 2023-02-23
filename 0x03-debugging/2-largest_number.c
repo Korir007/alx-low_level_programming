ea5423acb8fa31cc67f0e14fff66d215b82fae4c
dann <dann7@debian-BULLSEYE-live-builder-AMD64>
2023-02-23 12:33:39 +0300

    debugging 3rd challenge

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,12 +1,10 @@
-#include"main.h"
+#include "holberton.h"
 
 /**
  * largest_number - returns the largest of 3 numbers
- *
  * @a: first integer
  * @b: second integer
  * @c: third integer
- *
  * Return: largest number
  */
 
