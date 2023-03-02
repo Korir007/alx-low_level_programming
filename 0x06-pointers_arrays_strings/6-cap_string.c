047a73e9a9a6beab8de9d13c81d7cf0bdae6eb77
dann <dann7@debian-BULLSEYE-live-builder-AMD64>
2023-03-02 20:18:23 +0300

    tasks on more pointers

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,40 +1,30 @@
 #include "main.h"
-
 /**
- * cap_string - capitalizes all words of a string.
- * @s: string to use.
- *
- * Return: string.
+ * cap_string - capitalizes all words of a string
+ * @s: input string.
+ * Return: the pointer to dest.
  */
 
 char *cap_string(char *s)
 {
-	int i = 1, j, check;
-	char a[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\n', '\t', ' '};
+	int count = 0, i;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	if (s[0] > 96 && s[0] < 123)
-		s[0] -= 32;
-
-	while (s[i] != '\0')
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
 	{
-		if (s[i] > 96 && s[i] < 123)
+		for (i = 0; i < 13; i++)
 		{
-			j = 0;
-			check = 0;
-			while (check == 0 && j < 13)
+			if (*(s + count) == sep_words[i])
 			{
-				if (s[i - 1] == a[j])
-				{
-					check = 1;
-				}
-				j++;
-			}
-			if (check == 1)
-			{
-				s[i] -= 32;
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
 			}
 		}
-		i++;
+		count++;
 	}
 	return (s);
 }
