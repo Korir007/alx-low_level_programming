a47b88de4496f65c805177ddd8d09a3ebf94cadc
dann <dann7@debian-BULLSEYE-live-builder-AMD64>
2023-03-02 20:08:53 +0300

    tasks on more pointers

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,31 +1,27 @@
+#include "main.h"
 /**
- * _strncat - concatinate 2 strings.
- * 
- * @dest: first string.
- * @src: second string.
- * @n: the number of bytes to use from src.
- * 
- * Return: @dest
+ * _strncat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes used from src.
+ * Return: the pointer to dest.
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
-	int c, i;
+	int count = 0, count2 = 0;
 
-	c = 0;
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
 
-	/*find size of dest array*/
-	while (dest[c])
-		c++;
-
-	/**
-	 * src does not need to be null terminated
-	 * if it contains n or more bytes
-	*/
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[c + i] = src[i];
-	/*null terminate dest*/
-	dest[c + i] = '\0';
-
+	while (count2 < n)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
 	return (dest);
 }
