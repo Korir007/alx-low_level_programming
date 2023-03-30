81b8ec76e8d326a9979e49d8dbd7e769de542798
Korir007 <danielkorir303@gmail.com>
2023-03-30 23:55:59 +0300

    changes

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,20 @@
 section .data
-    msg db 'Hello, Holberton',0Ah ; message to be printed with new line character
-    len equ $-msg               ; length of message
+    hello db 'Hello, Holberton', 0
+    format db '%s\n', 0
 
 section .text
-    global _start
+    global main
+    extern printf
 
-_start:
-    ; write the message to stdout
-    mov eax, 4       ; syscall for write
-    mov ebx, 1       ; file descriptor for stdout
-    mov ecx, msg     ; address of message
-    mov edx, len     ; length of message
-    int 80h          ; call the kernel
+main:
+    push rbp
+    mov rbp, rsp
 
-    ; exit the program
-    mov eax, 1       ; syscall for exit
-    xor ebx, ebx     ; return code 0
-    int 80h          ; call the kernel
+    push qword hello
+    push qword format
+    call printf
+    add rsp, 16
+
+    mov rsp, rbp
+    pop rbp
+    ret
