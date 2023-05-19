6249657b70dae832934838d0fdaa674887a41b3e
Korir007 <danielkorir303@gmail.com>
2023-05-19 05:54:37 +0300

    tasks

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,31 +6,30 @@
  * @filename: name of file to create
  * @text_content: string to write to file
  *
- * Return: 1 on success OR -1 on faliure
+ * Return: 1 on success | on faliure
 */
 int create_file(const char *filename, char *text_content)
 {
 	int file, write_status, words = 0;
 
-	if (filename == NULL) /*check if filename is present*/
+	if (filename == NULL)
 		return (-1);
 
-	/*open file by creating it and if it exists write but truncate to 0*/
+
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (file == -1) /*check if file creation was a success*/
+	if (file == -1) 
 		return (-1);
 
-	if (text_content) /*write content to file if its not NULL*/
+	if (text_content) 
 	{
-		while (text_content[words] != '\0') /*find number of words*/
+		while (text_content[words] != '\0') 
 			words++;
 
-		/*write to file*/
 		write_status = write(file, text_content, words);
-		if (write_status == -1) /*check if write was a success*/
+		if (write_status == -1)
 			return (-1);
 	}
 
-	close(file); /*close file*/
+	close(file);
 	return (1);
 }
