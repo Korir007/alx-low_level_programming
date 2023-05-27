2143ef07beffa1e6181b55f2417e9e7a5493ffdb
Korir007 <danielkorir303@gmail.com>
2023-05-27 05:17:10 +0300

    minor fix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Korir007/alx-low_level_programming/blob/master/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp/ https://github.com/Korir007/alx-low_level_programming/blob/master/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD=/tmp/nrandom.so
